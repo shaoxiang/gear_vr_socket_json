5f508b1904a511f03be1193273b4595eb0d23563
shaoxiang <84854582@qq.com>
2015-11-01 11:14:14 +0800

    add python socket server
    add python socket server display the data client
    send to server

---
+++ python_socket_server.py
@@ -0,0 +1,47 @@
+import socket
+import sys
+from thread import *
+ 
+HOST = ''   # Symbolic name meaning all available interfaces
+PORT = 50000 # Arbitrary non-privileged port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'Socket created'
+ 
+#Bind socket to local host and port
+try:
+    s.bind((HOST, PORT))
+except socket.error , msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+     
+print 'Socket bind complete'
+ 
+#Start listening on socket
+s.listen(10)
+print 'Socket now listening'
+ 
+#Function for handling connections. This will be used to create threads
+def clientthread(conn):
+    #Sending message to connected client
+    conn.send('Welcome to the server. Type something and hit enter\n') #send only takes string
+    #infinite loop so that function do not terminate and thread do not end.
+    while True:
+		#Receiving from client
+        data = conn.recv(22400)
+        reply = 'OK...' + data
+        if not data: 
+            break
+        conn.sendall(reply)
+        print data
+		#came out of loop		
+    conn.close()
+#now keep talking with the client
+while 1:
+    #wait to accept a connection - blocking call
+    conn, addr = s.accept()
+    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+     
+    #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+    start_new_thread(clientthread ,(conn,))
+ 
+s.close()
